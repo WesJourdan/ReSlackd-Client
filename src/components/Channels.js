82cbe99573130c78d4c3da8760840b8e73caf681
Justin McCarty <jamccarty99@gmail.com>
2018-03-17 12:52:21 -0400

    finish stubbing out all components. boiler plate modal and channelsettings

---
+++ src/components/Channels.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import ChannelSettings from './ChannelSettings';
+
+class Channels extends Component {
+    render() {
+        return (
+          <div>Channels
+            <ChannelSettings />
+          </div>
+        );
+      }
+
+}
+
+export default Channels;
