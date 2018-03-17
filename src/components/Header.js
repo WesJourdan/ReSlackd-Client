fdb1176eff37b55f78c76cf5282626307f51c9c6
Justin McCarty <jamccarty99@gmail.com>
2018-03-17 11:45:00 -0400

    a few more small changes to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,17 @@ import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
+        console.log(this.props.auth);
+        
         switch (this.props.auth) {
           case null:
             return;
           case false:
             return <li><a href="/auth/google">Login With Google</a></li>;
           default:
-          return 
-            <li key="2"><a href="/api/logout">Logout</a></li>;
+          return [
+            <li><a href="/api/logout">Logout</a></li>
+          ];
         }
       }
 
@@ -20,7 +23,7 @@ class Header extends Component {
           <nav>
             <div className="nav-wrapper">
               <Link
-                to="/Messages"
+                to="/"
                 className="left brand-logo"
               >
                 ReSlackd
@@ -34,8 +37,8 @@ class Header extends Component {
       }
     }
     
-function mapStateToProps({ channels }) {
-    return { channels };
+function mapStateToProps({ auth }) {
+    return { auth };
 }
     
 export default connect(mapStateToProps)(Header);
