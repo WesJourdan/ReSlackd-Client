a9294872e64a0a555ce412c06def0c9c72e3753d
Wes Jourdan <wes.jourdan@icloud.com>
2018-03-17 09:31:08 -0400

    Added SingleMessage component and created a reference to its reducer in reducers/index.js

---
+++ src/components/SingleMessage.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import { connect } from 'react-redux';
+
+const MESSAGES_DUMMY_DATA = [
+	{ username: 'Wes', userId: '213', text: 'Hey guys!', timestamp: new Date(), image:'https://furtaev.ru/preview/user_3_small.png' },
+	{ username: 'Justin', userId: '234', text: 'Hello.', timestamp: new Date(), image: 'https://furtaev.ru/preview/user_3_small.png' },
+	{ username: 'Austin', userId: '432', text: 'Howdy!', timestamp: new Date(), image: 'https://furtaev.ru/preview/user_3_small.png' },
+	{ username: 'Ashley', userId: '123', text: 'Morning!', timestamp: new Date(), image: 'https://furtaev.ru/preview/user_3_small.png' },
+	{ username: 'John Doe', userId: '543', text: 'Huh?', timestamp: new Date(), image: 'https://furtaev.ru/preview/user_3_small.png' }
+]
+
+class SingleMessage extends Component {
+
+	render() {
+		return (
+			// these are the fields that we are rendering. Style is still not decided.
+			// for now we will be rendering dummy data.
+			// { this.props.messages.username }
+			// { this.props.messages.text }
+			// { this.props.messages.timestamp }
+			// { this.props.messages.image }
+			<div>
+				
+			</div>
+		);
+	};
+};
+
+function mapStateToProps({ messages }) {
+	return { messages }
+};
+
+export default connect(mapStateToProps)(SingleMessage);
