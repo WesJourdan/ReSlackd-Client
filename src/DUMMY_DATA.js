dc611e04c9c3761fcd5908afa9ead9ffca22501d
Justin McCarty <jamccarty99@gmail.com>
2018-03-18 17:05:47 -0400

    fix naming of uID and name in dummy data

--- src/DUMMY_DATA.js
+++ src/DUMMY_DATA.js
@@ -1,17 +1,17 @@
 export const MESSAGES = [
-  { "username": "Isadore49", "userId": "747c3db5-15ac-4e16-a1eb-6f0f69485c95", "text": "Tempora tempora voluptatem velit ipsum et doloribus.", "timestamp": 1521291706742, "imageURL": "http://lorempixel.com/640/480" },
-  { "username": "Monserrat65", "userId": "7176f315-3333-4988-97e8-5594277fb2a0", "text": "Et aut nulla.", "timestamp": 1521226808569, "imageURL": "http://lorempixel.com/640/480" },
-  { "username": "Riley57", "userId": "2fd39c89-b81c-482f-8deb-cad9d6008f0b", "text": "Non necessitatibus consequatur et nemo.", "timestamp": 1521298911025, "imageURL": "http://lorempixel.com/640/480" },
-  { "username": "Ahmed23", "userId": "16cfe06c-49ce-4291-942f-7470e32516f4", "text": "Unde recusandae ducimus porro qui ipsa saepe minus.", "timestamp": 1521255160085, "imageURL": "http://lorempixel.com/640/480" },
-  { "username": "Virgil68", "userId": "c1187885-a86b-4570-a857-19b62813daaf", "text": "Aut voluptas sed voluptas ea ipsam occaecati expedita quo.", "timestamp": 1521276220750, "imageURL": "http://lorempixel.com/640/480" },
-  { "username": "Christine.Herzog", "userId": "0383d98c-7785-4e31-9d57-eb48b99c3ab2", "text": "Unde ducimus harum asperiores.", "timestamp": 1521254195067, "imageURL": "http://lorempixel.com/640/480" },
-  { "username": "Ralph16", "userId": "904c691b-783f-45d1-8b95-581fc7c85395", "text": "Voluptatem repellat possimus placeat quis ipsam aut.", "timestamp": 1521249398249, "imageURL": "http://lorempixel.com/640/480" },
-  { "username": "Onie_Reynolds60", "userId": "17b8b318-9a5f-4769-bb51-c93c4a62f541", "text": "Temporibus explicabo consequatur et a maxime dolor dolor.", "timestamp": 1521288179089, "imageURL": "http://lorempixel.com/640/480" },
-  { "username": "Nash_Russel", "userId": "846ac9eb-acc3-44e9-9368-f4b242c77f78", "text": "Laboriosam adipisci unde qui.", "timestamp": 1521260020596, "imageURL": "http://lorempixel.com/640/480" },
-  { "username": "Jabari_Yundt", "userId": "0b72e7cd-767b-445a-a06b-ccde7995fd6d", "text": "Temporibus fugiat minima earum aut magni quibusdam illum.", "timestamp": 1521241864582, "imageURL": "http://lorempixel.com/640/480" },
-  { "username": "Naomi8", "userId": "682fbadc-4731-47b8-b1c0-9d19b3ab2ec0", "text": "Asperiores fuga sunt ex animi aut iure quis.", "timestamp": 1521267920064, "imageURL": "http://lorempixel.com/640/480" },
-  { "username": "Ella_Considine", "userId": "bbc8fb17-3f9c-4ceb-9a13-89842fb91f75", "text": "Aut sed perspiciatis autem aut ut voluptatum perferendis sint.", "timestamp": 1521223808259, "imageURL": "http://lorempixel.com/640/480" },
-  { "username": "Dorian75", "userId": "e0147124-0e37-4168-9b12-6c763fc56932", "text": "Unde autem accusamus qui et.", "timestamp": 1521251002251, "imageURL": "http://lorempixel.com/640/480" }
+  { "name": "Isadore49", "uID": "747c3db5-15ac-4e16-a1eb-6f0f69485c95", "text": "Tempora tempora voluptatem velit ipsum et doloribus.", "timestamp": 1521291706742, "imageURL": "http://lorempixel.com/640/480" },
+  { "name": "Monserrat65", "uID": "7176f315-3333-4988-97e8-5594277fb2a0", "text": "Et aut nulla.", "timestamp": 1521226808569, "imageURL": "http://lorempixel.com/640/480" },
+  { "name": "Riley57", "uID": "2fd39c89-b81c-482f-8deb-cad9d6008f0b", "text": "Non necessitatibus consequatur et nemo.", "timestamp": 1521298911025, "imageURL": "http://lorempixel.com/640/480" },
+  { "name": "Ahmed23", "uID": "16cfe06c-49ce-4291-942f-7470e32516f4", "text": "Unde recusandae ducimus porro qui ipsa saepe minus.", "timestamp": 1521255160085, "imageURL": "http://lorempixel.com/640/480" },
+  { "name": "Virgil68", "uID": "c1187885-a86b-4570-a857-19b62813daaf", "text": "Aut voluptas sed voluptas ea ipsam occaecati expedita quo.", "timestamp": 1521276220750, "imageURL": "http://lorempixel.com/640/480" },
+  { "name": "Christine.Herzog", "uID": "0383d98c-7785-4e31-9d57-eb48b99c3ab2", "text": "Unde ducimus harum asperiores.", "timestamp": 1521254195067, "imageURL": "http://lorempixel.com/640/480" },
+  { "name": "Ralph16", "uID": "904c691b-783f-45d1-8b95-581fc7c85395", "text": "Voluptatem repellat possimus placeat quis ipsam aut.", "timestamp": 1521249398249, "imageURL": "http://lorempixel.com/640/480" },
+  { "name": "Onie_Reynolds60", "uID": "17b8b318-9a5f-4769-bb51-c93c4a62f541", "text": "Temporibus explicabo consequatur et a maxime dolor dolor.", "timestamp": 1521288179089, "imageURL": "http://lorempixel.com/640/480" },
+  { "name": "Nash_Russel", "uID": "846ac9eb-acc3-44e9-9368-f4b242c77f78", "text": "Laboriosam adipisci unde qui.", "timestamp": 1521260020596, "imageURL": "http://lorempixel.com/640/480" },
+  { "name": "Jabari_Yundt", "uID": "0b72e7cd-767b-445a-a06b-ccde7995fd6d", "text": "Temporibus fugiat minima earum aut magni quibusdam illum.", "timestamp": 1521241864582, "imageURL": "http://lorempixel.com/640/480" },
+  { "name": "Naomi8", "uID": "682fbadc-4731-47b8-b1c0-9d19b3ab2ec0", "text": "Asperiores fuga sunt ex animi aut iure quis.", "timestamp": 1521267920064, "imageURL": "http://lorempixel.com/640/480" },
+  { "name": "Ella_Considine", "uID": "bbc8fb17-3f9c-4ceb-9a13-89842fb91f75", "text": "Aut sed perspiciatis autem aut ut voluptatum perferendis sint.", "timestamp": 1521223808259, "imageURL": "http://lorempixel.com/640/480" },
+  { "name": "Dorian75", "uID": "e0147124-0e37-4168-9b12-6c763fc56932", "text": "Unde autem accusamus qui et.", "timestamp": 1521251002251, "imageURL": "http://lorempixel.com/640/480" }
 ]
 
 export const CHANNEL_LIST = [
