b8afd261df96d1a573acdd292752e21378e6e1d6
Austin Stevens <austinestevens@outlook.com>
2018-03-19 11:02:46 -0400

    Added responsive sidebar menu, custom font, and support for Fontawesome icons. Also refactored to reuse Channels component for channels and direct messages.

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,76 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../actions';
 import './App.css';
 import Header from './Header';
 import MessageBoard from './MessageBoard';
-import DirectMessages from './DirectMessages'
 import Channels from './Channels';
+import Sidebar from 'react-sidebar';
+import fontawesome, { library } from '@fortawesome/fontawesome'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import regular from '@fortawesome/fontawesome-free-regular'
+import solid from '@fortawesome/fontawesome-free-solid'
 
+fontawesome.library.add(regular, solid)
 
+const mediaQueryLimit = window.matchMedia(`(min-width: 800px`);
 
 class App extends Component {
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      mediaQueryLimit,
+      docked: props.docked,
+      open: props.open
+    }
+  }
+
+  onSetSidebarOpen = (open) => {
+    this.setState({sidebarOpen: open});
+  }
+
+  mediaQueryChanged = () => {
+    this.setState({sidebarDocked: this.state.mediaQueryLimit.matches})
+  }
+
+  componentWillMount() {
+    mediaQueryLimit.addListener(this.mediaQueryChanged);
+    this.setState({mediaQueryLimit, sidebarDocked: mediaQueryLimit.matches})
+  }
+
+  componentWillUnmount() {
+    this.state.mediaQueryLimit.removeListener(this.mediaQueryChanged);
+  }
+
   render() {
+
     return (
       <div className="">
-            <Header />
-            <Channels />
-            <DirectMessages />
-            <MessageBoard />
+        <Header />
+        <Sidebar
+          sidebar={
+            <div className='mr-4'>
+              <Channels messageType='channel' />
+              <Channels messageType='directMessage' />
+            </div>
+          }
+          open={this.state.sidebarOpen}
+          docked={this.state.sidebarDocked}
+          onSetOpen={this.onSetSidebarOpen}
+          shadow={false}
+          styles={{
+            sidebar: {
+              top: '4rem'
+            },
+            content: {
+              top: '4rem'
+            }
+          }}
+        >
+          <MessageBoard />
+        </Sidebar>
       </div>
     );
   }
