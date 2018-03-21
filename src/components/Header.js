30d73603c8e4c3500246a979046d18774f223c01
Ashley Hardin <ashleyhardin@ashleys-air.attlocal.net>
2018-03-21 19:18:46 -0400

    messages showing in the correct order

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,6 @@ import { connect } from 'react-redux';
 import { fetchCurrentUser } from '../actions';
 import { bindActionCreators } from "redux";
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import { bindActionCreators } from "redux";
-import { fetchCurrentUser } from '../actions';
 
 class Header extends Component {
 
@@ -46,14 +44,7 @@ function mapStateToProps({ auth }) {
 }
 
 function mapDispatchToProps(dispatch) {
-<<<<<<< HEAD
   return bindActionCreators({ fetchCurrentUser }, dispatch);
 }
-    
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
-=======
-  return bindActionCreators({ fetchCurrentUser  }, dispatch)
-};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
->>>>>>> 37ee0938ac956fe48e00f2e370756a3fe93d8826
